7202b465bf5548d55800b6ff2861086230f94d90
Dasha <darac0816@gmail.com>
2024-03-25 19:22:31 +0300

    Second version. Bot can print 10 last messages from users

--- botyara.py
+++ botyara.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 import os
 
 
-class SimpleNeuralNetwork(nn.Module):
+class SummarizationNeuralNetwork(nn.Module):
     def __init__(self):
-        super(SimpleNeuralNetwork, self).__init__()
+        super(SummarizationNeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(1, 1)
 
     def forward(self, x):
@@ -23,14 +23,40 @@ bot = telebot.TeleBot(os.getenv('TOKEN'))
 with open('forbw.txt', 'r', encoding='utf8') as file:
     forbidden_words = [word.strip().lower().replace(', ', ' ') for word in file.readlines()]
 
-net = SimpleNeuralNetwork()
+net = SummarizationNeuralNetwork()
 optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
 conversation_memory = []
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет! Я бот, созданный на python.")
+    bot.send_message(message.chat.id, "Привет!")
+
+
+@bot.message_handler(commands=['summarize'])
+def ask_num_messages(message):
+    bot.send_message(message.chat.id, "Сколько сообщений нужно суммаризировать?")
+    bot.register_next_step_handler(message, summarize_messages)
+
+
+def summarize_messages(message):
+    try:
+        num_messages = int(message.text)
+        if num_messages <= 0:
+            bot.send_message(message.chat.id, "Некорректное количество сообщений. Введите положительное число.")
+            return
+        preprocessed_text = ', '.join(conversation_memory[-num_messages:]).lower()
+        summarized_topics = summarize_topics(net, preprocessed_text,
+                                             num_messages=num_messages)
+        bot.send_message(message.chat.id, f"Темы: {summarized_topics}")
+    except ValueError:
+        bot.send_message(message.chat.id, "Некорректное количество сообщений. Введите положительное число.")
+
+
+def summarize_topics(net, text, num_messages=None):
+    topics = text.split(', ')[0:num_messages]
+    return ', '.join(topics)
 
 
 @bot.message_handler(func=lambda message: True)
@@ -38,19 +64,20 @@ def echo_all(message):
     global conversation_memory
 
     conversation_memory.append(message.text)
+    conversation_memory = conversation_memory[-10:]
 
     for word in (' '.join(forbidden_words)).split():
         if word in message.text.lower():
             bot.send_message(message.chat.id, "Эй, у нас не одобряются такие выражения!")
             return
-    if message.text.lower() == 'привет':
+    if 'привет' in message.text.lower():
         bot.send_message(message.chat.id, "Привет, как дела?")
-    elif message.text.lower().replace('?', '') == 'кто лучший дизайнер в мире':
+    elif 'кто лучший дизайнер в мире' in message.text.lower().replace('?', ''):
         bot.send_message(message.chat.id, "Дарик, что за вопросы, она вообще лучшая)")
     elif 'дарик' in message.text.lower():
         bot.send_message(message.chat.id, "Я увидел ЕЁ имя? Дарик лучшая!!")
-    else:
-        bot.send_message(message.chat.id, "Я не понимаю, что ты имеешь в виду.")
+    elif 'a*' in message.text.lower():
+        bot.send_message(message.chat.id, "ГЕНИИ, ПОБЕДИТЕЛИ, ЛУЧШИЕ!")
 
 
 bot.polling()
